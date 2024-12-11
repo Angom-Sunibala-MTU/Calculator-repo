95ad2ee831ae3dd6e63c6a69a8e596d3a2b1d16e
Sunibala <angomsunibala1@gmail.com>
2024-12-11 07:29:46 +0530

    This is a calculator program

---
+++ Calculator-program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+  
+
+int main()
+{ 
+	int number1,number2,sum;
+
+
+	//Asking the user to enter two integers
+	printf("Enter two integers: ");
+	scanf("%d %d",&number1,&number2);
+
+	//calculating the sum
+	sum=number1+number2;
